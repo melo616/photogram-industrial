c4405718264997a837cb6b74560e5fba51193ef2
Katherine Jarboe <110368535+melo616@users.noreply.github.com>
2023-10-19 16:26:37 +0000

    Added indirect associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,5 +34,18 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: :fan_id
 
   has_many :sent_follow_requests, foreign_key: :sender_id, class_name: "FollowRequest"
+  has_many :accepted_sent_follow_requests, -> { where(status: "accepted") }, foreign_key: :sender_id, class_name: "FollowRequest"
   has_many :received_follow_requests, foreign_key: :recipient_id, class_name: "FollowRequest"
+  has_many :accepted_received_follow_requests, -> { where(status: "accepted") }, foreign_key: :recipient_id, class_name: "FollowRequest"
+
+  #indirect associations
+  #shows all the photos the user has liked
+  has_many :liked_photos, through: :likes, source: :photos
+  #recipients of sent follow requests from the user
+  has_many :leaders, through: :accepted_sent_follow_requests, source: :recipient
+  #accepted follow requests received by the user aka followers
+  has_many :followers, through: :accepted_received_follow_requests, source: :sender
+
+  has_many :feed, through: :leaders, source: :own_photos
+  has_many :discover, through: :leaders, source: :liked_photos
 end
